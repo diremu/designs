9da499c268b9d61c3d2b2f73b75b748cae2ed8c5
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-07-10 21:21:35 +0100

    further changes to the logic

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
 let buttons = document.querySelectorAll("button");
 let output = document.querySelector("#output").value;
+let numbers = document.querySelectorAll(".digits")
 
 for (let i=0; i < buttons.length;i++) {
     buttons[i].addEventListener("click", handleClick);
 }
 
 function handleClick(e) {
-    
+    switch (e.target.dataset.value) {
+        case "clear":
+            output = ""
+            break;
+        case "clearAll":
+            output = ""
+            break;
+        case "delete":
+            let outputString = output.split('');
+            outputString.pop()
+            output = outputString.join('')
+            break;
+        case "/":
+    }
 }
