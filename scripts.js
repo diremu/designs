24fb49cc6b0d1e718d8de4ca0fd86ba4fef05ac0
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-07-12 10:10:13 +0100

    Completed the special commands

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let buttons = document.querySelectorAll("button");
 let output = document.querySelector("#output");
-let numbers = document.querySelectorAll(".digits")
+let numbers = document.querySelectorAll(".digits");
+let prevOutput = document.querySelector("#prevOutput")
+let addArray = [];
 
 for (let i=0; i < buttons.length;i++) {
     buttons[i].addEventListener("click", handleClick);
@@ -12,12 +14,13 @@ function handleClick(e) {
             output.value = '';
             break;
         case "clearAll":
-            output = ""
+            output.value = '';
+            prevOutput.value = '';
             break;
         case "delete":
-            let outputString = output.split('');
-            outputString.pop()
-            output = outputString.join('')
+            let newArray = output.value.split('');
+            newArray.pop()
+            output.value = newArray.join('')
             break;
         default:
             alert('Use a proper command');
