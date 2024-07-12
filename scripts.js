4dd5906effd06fd4e177e2a2cde91ab45e20f6f6
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-07-12 14:15:06 +0100

    Completed all except the equals, dot and change operators

--- scripts.js
+++ scripts.js
@@ -1,29 +1,43 @@
 let buttons = document.querySelectorAll("button");
 let output = document.querySelector("#output");
 let numbers = document.querySelectorAll(".digits");
-let prevOutput = document.querySelector("#prevOutput")
-let addArray = [];
+let prevOutput = document.querySelector("#prevOutput");
+let addArray = output.value.split("");
+let prevArray = prevOutput.value.split('');
 
-for (let i=0; i < buttons.length;i++) {
-    buttons[i].addEventListener("click", handleClick);
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", handleClick);
 }
 
 function handleClick(e) {
-    switch (e.target.dataset.value) {
-        case "clear":
-            output.value = '';
-            break;
-        case "clearAll":
-            output.value = '';
-            prevOutput.value = '';
-            break;
-        case "delete":
-            let newArray = output.value.split('');
-            newArray.pop()
-            output.value = newArray.join('')
-            break;
-        default:
-            alert('Use a proper command');
-            break;
-    }
+  switch (e.target.dataset.value) {
+    case "clear":
+      output.value = "";
+      addArray = [''];
+      break;
+    case "clearAll":
+      output.value = "";
+      prevOutput.value = "";
+      prevArray = [''];
+      addArray = [''];
+      break;
+    case "delete":
+      let newArray = output.value.split("");
+      newArray.pop();
+      output.value = newArray.join("");
+      break;
+    case "digit":
+      addArray.push(e.target.dataset.class);
+      output.value = addArray.join('')
+      break;
+    case "operator":
+        prevArray = [...prevArray, output.value, e.target.dataset.class ]
+        prevOutput.value = prevArray.join(' ')
+        output.value = ' '
+        addArray = ['']
+        break;
+    default:
+      alert("Use a proper command");
+      break;
+  }
 }
